f308706e3b2f5ec50c2453d973a831673e185e81
pandyandy <and.reanovakova@seznam.cz>
2025-05-07 19:36:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,6 +49,9 @@ icons=['pin-map-fill', 'people', 'file-bar-graph', 'chat-heart', 'robot'] #'info
 menu_id = option_menu(None, options=options, icons=icons, key='menu_id', orientation="horizontal")
 
 
+# Convert ENTITY and ATTRIBUTE columns to lowercase
+attributes['ENTITY'] = attributes['ENTITY'].str.lower()
+attributes['ATTRIBUTE'] = attributes['ATTRIBUTE'].str.lower()
 
 #attributes['entity'] = attributes['entity'].replace('burgers', 'burger')
 pronouns_to_remove = ['i', 'you', 'she', 'he', 'it', 'we', 'they', 'I', 'You', 'She', 'He', 'It', 'We', 'They']
